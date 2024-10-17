7f9d44bbe07f6c9656e0ced20b909719911a3356
cseth1 <147735639+cseth1@users.noreply.github.com>
2024-10-17 12:08:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Loading from './components/Loading'; // Import the Loading component
+import Loading from './components/Loading';
 
 // Lazy load pages
 const HomePage = lazy(() => import('./pages/HomePage'));
@@ -14,12 +14,11 @@ function App() {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
-          <Suspense fallback={<Loading />}> {/* Show loading spinner while routes are being lazy loaded */}
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/course/:id" component={CourseDetailPage} />
-              {/* Add other routes as needed */}
-            </Switch>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} /> {/* Update to element={} */}
+              <Route path="/course/:id" element={<CourseDetailPage />} />
+            </Routes>
           </Suspense>
         </main>
         <Footer />
