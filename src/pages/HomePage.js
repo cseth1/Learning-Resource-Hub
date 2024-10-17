8761d370cdec96a29f91df6c4811f3ba9d41e817
cseth1 <147735639+cseth1@users.noreply.github.com>
2024-10-17 11:59:34 -0500

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import CourseCards from '../components/CourseCards';
 
 function HomePage() {
   return (
     <div>
-      <Header />
       <HeroSection />
       <CourseCards />
       {/* Other sections of your homepage */}
@@ -15,3 +13,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
